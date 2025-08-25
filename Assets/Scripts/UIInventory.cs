4528bf0f0aed1c9b81f50426b78e32c1fcb821d8
SeungYeop Oh <sadrain9274@naver.com>
2025-08-25 21:25:29 +0900

    [feat] 상태창, 인벤토리창 뒤로가기 버튼 추가

--- Assets/Scripts/UIInventory.cs
+++ Assets/Scripts/UIInventory.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIInventory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Button backBtn;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        backBtn.onClick.AddListener(() => UIManager.Instance.MainMenu.OpenMainMenu());
     }
 }
--- Assets/Scripts/UIStatus.cs
+++ Assets/Scripts/UIStatus.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIStatus : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Button backBtn;
+    private void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        backBtn.onClick.AddListener(() => UIManager.Instance.MainMenu.OpenMainMenu());
     }
 }
