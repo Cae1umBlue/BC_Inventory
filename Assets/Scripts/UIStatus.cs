0dbddc9e8ab217f3c8a9dff03d49f8004a2a7a4e
SeungYeop Oh <sadrain9274@naver.com>
2025-08-25 21:37:46 +0900

    [feat] UIStatus 필드변수 추가

--- Assets/Scripts/UIStatus.cs
+++ Assets/Scripts/UIStatus.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIStatus : MonoBehaviour
 {
     [SerializeField] private Button backBtn;
+
+    [SerializeField] private TextMeshProUGUI attackValue;
+    [SerializeField] private TextMeshProUGUI defenceValue;
+    [SerializeField] private TextMeshProUGUI healthValue;
+    [SerializeField] private TextMeshProUGUI criticalValue;
     private void Start()
     {
         backBtn.onClick.AddListener(() => UIManager.Instance.MainMenu.OpenMainMenu());
