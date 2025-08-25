6973cbbbfeb381c41d4b2fc062da03e3f3e7fb21
SeungYeop Oh <sadrain9274@naver.com>
2025-08-25 19:15:41 +0900

    [feat] UIMainMenu 클래스 필드변수 선언

--- Assets/Scripts/UIMainMenu.cs
+++ Assets/Scripts/UIMainMenu.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIMainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    /* 작성 목록
+    ID(닉네임) : TextMeshproGUI
+    LV(레벨)
+    Description(캐릭터 설명)
+    Money(소지재화)
+    StatueBtn(스텟창 on 버튼) : Gameobject
+    Inventory(인벤토리창 on 버튼) : Gameobject
+    */
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private TextMeshProUGUI id;
+    [SerializeField] private TextMeshProUGUI level;
+    [SerializeField] private TextMeshProUGUI description;
+    [SerializeField] private TextMeshProUGUI money;
+    [SerializeField] private GameObject statusBtn;
+    [SerializeField] private GameObject inventoryBtn;
+
+    [SerializeField] private GameObject uiStatus;
+    [SerializeField] private GameObject uiInventory;
 }
