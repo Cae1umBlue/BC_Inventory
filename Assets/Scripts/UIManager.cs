8154f6fa547156c78e4951999f225e9af6eef7c2
SeungYeop Oh <sadrain9274@naver.com>
2025-08-25 17:13:19 +0900

    [feat] UIManager 하위 UI스크립트 연결

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class UIManager : Singleton<UIManager>
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject uiMainMenu;
+    [SerializeField] GameObject uiStatus;
+    [SerializeField] GameObject uiInventory;
 
-    // Update is called once per frame
-    void Update()
+    protected override void Initialize()
     {
-        
+        base.Initialize();
     }
 }
