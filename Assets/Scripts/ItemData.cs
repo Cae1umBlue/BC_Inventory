4e5c6eed20320332801101c81407b20a59144a81
SeungYeop Oh <sadrain9274@naver.com>
2025-08-26 20:37:26 +0900

    [feat] 아이템데이터 스크립터블 오브젝트 스크립트 추가

---
+++ Assets/Scripts/ItemData.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Item/ItemData")]
+public class ItemData : ScriptableObject
+{
+    public int id;
+    public string itemName;
+    public string description;
+    public Sprite icon;
+    public int price;
+
+    public int extraAttack;
+    public int extraDefence;
+    public int extraHealth;
+    public int extraCritical;
+}
