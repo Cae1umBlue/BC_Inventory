ce1ca1b93ab934079c4e65805ba3bb6c6738aea4
SeungYeop Oh <sadrain9274@naver.com>
2025-08-27 17:39:35 +0900

    [feat] 게임매니저에서 아이템 데이터 생성

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,13 +17,25 @@ public class GameManager : Singleton<GameManager>
     public void SetData()
     {
         Player = new Character("Mina", 01, "각종 무술 유단자", 10, 5, 100, 3, 1000);
-
-        ItemData cloth = Resources.Load<ItemData>("ItemDatas/Cloth");
-
-        Player.Additem(cloth);
-
+        SetInventoryItem();    
         uiManager.Status.Initialize(Player);
         uiManager.MainMenu.Initialize(Player);
         uiManager.Inventory.Initialize(Player);
+    }
+
+    public void SetInventoryItem() // 인벤토리 아이템 데이터 추가
+    {
+        string[] itemNames = {
+        "axe", "belts", "boots", "gloves", "helmets",
+        "necklace", "pants", "rings", "shield", "shoulders", "sword"
+        };
+
+        foreach (var itemName in itemNames)
+        {
+            ItemData item = Resources.Load<ItemData>($"ItemDatas/{itemName}");
+
+            if (item != null)
+                Player.Additem(item);
+        }
     }
 }
